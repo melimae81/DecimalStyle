bd51e248a0d1d9599fe1c500583814402634c234
melimae81 <melimae@gmail.com>
2018-04-02 16:09:27 -0400

    Updated events

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# Mendix Widget Boilerplate
+# Decimal Style
+This widget will format a decimal.
 
 See [AppStoreWidgetBoilerplate](https://github.com/mendix/AppStoreWidgetBoilerplate/) for an example
--- src/DecimalStyle/widget/DecimalStyle.js
+++ src/DecimalStyle/widget/DecimalStyle.js
@@ -1,7 +1,7 @@
 define([
     "dojo/_base/declare",
     "mxui/widget/_WidgetBase",
-
+    "dijit/_TemplatedMixin",
     "mxui/dom",
     "dojo/dom",
     "dojo/dom-prop",
@@ -14,13 +14,26 @@ define([
     "dojo/text",
     "dojo/html",
     "dojo/_base/event",
+    "dojo/text!DecimalStyle/widget/template/DecimalStyle.html"
 
 
-], function (declare, _WidgetBase, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent) {
+], function (declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoProp, dojoGeometry, dojoClass, dojoStyle, dojoConstruct, dojoArray, lang, dojoText, dojoHtml, dojoEvent, widgetTemplate) {
     "use strict";
 
-    return declare("DecimalStyle.widget.DecimalStyle", [ _WidgetBase ], {
+    return declare("DecimalStyle.widget.DecimalStyle", [_WidgetBase, _TemplatedMixin], {
 
+
+        templateString: widgetTemplate,
+        widgetBase: null,
+
+        //nodes
+        beforeNode: null,
+        afterNode: null,
+        decimalNode: null,
+        field: null,
+        beforeClassName: null,
+        afterClassName: null,
+        onclickMicroflow: null,
 
         // Internal variables.
         _handles: null,
@@ -32,15 +45,54 @@ define([
 
         postCreate: function () {
             logger.debug(this.id + ".postCreate");
+            this._setupEvents();
         },
 
         update: function (obj, callback) {
             logger.debug(this.id + ".update");
 
             this._contextObj = obj;
+            this._resetSubscriptions();
             this._updateRendering(callback);
         },
 
+        _setupEvents: function () {
+            this.connect(this.domNode, "onclick", function () {
+                mx.data.action({
+                    params: {
+                        applyto: "selection",
+                        actionname: this.onclickMicroflow,
+                        guids: [this._contextObj.getGuid()]
+                    },
+                    origin: this.mxform,
+                    callback: function (obj) {
+                        // no MxObject expected
+                        console.log("microflow ran successfully")
+                    },
+                    error: function (error) {
+                        alert(error.message);
+                    },
+                    onValidation: function (validations) {
+                        alert("There were " + validation.length + " validation errors");
+                    }
+                });
+            });
+        },
+
+        _resetSubscriptions: function () {
+            this.unsubscribeAll();
+            //add an attribute subscription
+            this.subscribe({
+                guid: this._contextObj.getGuid(),
+                attr: this.field,
+                callback: this._updateRendering
+            }),
+                this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    callback: this._updateRendering
+                })
+            //add object subscription
+        },
         resize: function (box) {
             logger.debug(this.id + ".resize");
         },
@@ -54,6 +106,12 @@ define([
 
             if (this._contextObj !== null) {
                 dojoStyle.set(this.domNode, "display", "block");
+                var value = "" + this._contextObj.get(this.field) * 1;
+                var splitValues = value.split(".");
+                this.beforeNode.innerHTML = splitValues[0];
+                this.beforeNode.className += " " + this.beforeClassName;
+                this.afterNode.innerHTML = splitValues[1];
+                this.afterNode.className += " " + this.afterClassName;
             } else {
                 dojoStyle.set(this.domNode, "display", "none");
             }
